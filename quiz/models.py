e1c7a6e20498d4d84df662f0151e07b2d8e31f46
Kushal <your_email@example.com>
2025-10-08 18:48:05 +0545

    Added the quiz models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,31 @@
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    option_a = models.CharField(max_length=200)
+    option_b = models.CharField(max_length=200)
+    option_c = models.CharField(max_length=200)
+    option_d = models.CharField(max_length=200)
+    correct_answer = models.CharField(
+        max_length=1,
+        choices=[("A", "A"), ("B", "B"), ("C", "C"), ("D", "D")]
+    )
+
+    def __str__(self):
+        return self.question_text
+
+
+class Score(models.Model):
+    user = models.CharField(max_length=100)  # OR link to User with ForeignKey
+    points = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.points}"
+
+
