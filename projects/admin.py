0719458b5458ad51f451a58df5ab8e8ea8c44abb
Kushal <your_email@example.com>
2025-10-08 14:08:52 +0545

    Updated the admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Project
 # Register your models here.
-admin.site.register(Project)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "technologies", "created_at")
+    search_fields = ("title", "technologies")
