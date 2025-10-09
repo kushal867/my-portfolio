288267708c24ea64fee643e59ade947677e38b50
Kushal <your_email@example.com>
2025-10-09 15:45:33 +0545

    feat: Added the forms.py

--- acoount/forms.py
+++ acoount/forms.py
@@ -1,26 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# forms.ModelForm ==> model define
-
-class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Username"}, ))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
-
-
-    def clean_username(self):
-        username= self.cleaned_data['username']
-        if len(username)<=3:
-            raise forms.ValidationError("Username length should be greater than 4")
-        return username
-
-    def clean_password(self):
-        password = self.cleaned_data['password']
-        return password
-
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['first_name','last_name','username','password']
+        fields = ["username", "email", "password1", "password2"]
