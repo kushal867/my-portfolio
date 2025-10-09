5bd86a1141b3ed3b094f28b5e082822104fc1c7a
Kushal <your_email@example.com>
2025-10-09 15:46:18 +0545

    feat: Added the urls.py

--- acoount/urls.py
+++ acoount/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import register, user_login, user_defined_login
-urlpatterns = [
-    path('',user_login,name="user-login"),
-    path('userlogin',user_defined_login, name="user-login"),
-    path('register',register, name="user-register"),
+from . import views
 
+urlpatterns = [
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
