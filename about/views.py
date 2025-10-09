549735209bf50660489e868d2c014ebee6087139
Kushal <your_email@example.com>
2025-10-09 18:05:32 +0545

    feat: updated ui and ux

--- about/views.py
+++ about/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from projects.models import Project
 
 def home(request):
-    return render(request, "home.html")
+    recent_projects = Project.objects.order_by('-created_at')[:3]
+    return render(request, "home.html", {"recent_projects": recent_projects})
