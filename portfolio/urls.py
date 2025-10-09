31d7064115b9a12d29591884bab4eedf96da4b17
Kushal <your_email@example.com>
2025-10-09 17:07:18 +0545

    feat: remove the duplicacy

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("api/quiz/", include("quiz.urls_api")),
     path("blog/", include("blog.urls")),
     path("acoount/", include("acoount.urls")),
+    path("accounts/", include("acoount.urls")),
     path("api/blog/", include("blog.urls_api")),
    
 ]
